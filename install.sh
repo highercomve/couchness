73fc5bb8fc842ada1c729b09467ee46ee34a7ae9
Sergio Marin <higher.vnf@gmail.com>
2023-05-26 22:12:48 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-  TAG=master
-else
-  TAG=$1
-fi
+install_location=${1:-"/usr/bin/couchness"}
 
 case $(uname -m | sed 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/') in
   *64)
@@ -43,6 +39,29 @@ esac
 PLATFORM=($OS"_"$ARCH)
 
 echo "Downloading couchness build for $PLATFORM to ~/bin/couchness"
-wget -O $HOME/bin/couchness https://raw.githubusercontent.com/highercomve/couchness/${TAG}/build/${PLATFORM}/couchness
 
-chmod +x $HOME/bin/couchness
+version=`curl -s -L 'https://api.github.com/repos/highercomve/couchness/releases' -H 'Accept: application/vnd.github.v3+json' -H 'Accept-Language: en-US,en;q=0.5' | jq --arg platform "$PLATFORM" '[.[] | select(.prerelease == false)][0].assets | (.[] | select(.name | contains("'$platform'.zip")))'`
+downloadurl=`echo "$version" | jq -r .browser_download_url`
+versionnumber=`echo "$version" | jq -r .name`
+
+installed_bin=`whereis couchness | awk '{print $2}'`
+if [ "$installed_bin" != "" ]; then
+  echo
+  echo "You have couchness installed"
+  $installed_bin -v
+fi
+
+echo ""	
+echo "Do you what to download $downloadurl [yes/y]"
+
+read download
+
+if [ "$download" != "yes" ] && [ "$download" != y ]; then
+	exit 0
+fi
+
+echo "Downloading latest version from: $downloadurl"
+
+curl -L "$downloadurl" -o "$install_location"
+
+chmod +x "$install_location"
